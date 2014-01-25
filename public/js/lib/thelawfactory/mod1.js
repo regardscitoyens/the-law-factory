03eb255748a67de02769cb3cbe0c6ee3e975e3c1
RouxRC <b.ooghe@gmail.com>
2014-01-26 00:35:15 +0100

    add % modifs info

--- public/js/lib/thelawfactory/mod1.js
+++ public/js/lib/thelawfactory/mod1.js
@@ -527,9 +527,9 @@
                 section = da.section,
                 status = d['id_step'].replace(/_/g, ", "),
                 length = d['length'];
-            $(".art-meta").html((section != null ? "<p><b>Section :</b> "+section+"</p>" : "")+"<p><b>Étape :</b> "+status+"</p><p><b>"+(d['status'] == "sup" ? "Supprimé à cette étape." : "Longueur du texte :</b> "+length)+"</p><p><b>Alinéas :</b></p>")
+            $(".art-meta").html((section != null ? "<p><b>Section :</b> "+section+"</p>" : "")+"<p><b>Étape :</b> "+status+"</p><p><b>"+(d['status'] == "sup" ? "Supprimé à cette étape." : "<p><b>Modifications :</b> "+d3.round(d['n_diff']*100, 2)+"&nbsp;%</p><p>Longueur du texte :</b> "+length)+"</p><p><b>Alinéas :</b></p>")
             $("#law-title").text("Article "+datum.datum().titre);
-            $(".art-txt").html("<ul><li><span>"+$.map(d.textDiff, function(i){return i.replace(/ (:»;\?!%€)/, '&nbsp;$1')}).join("</span></li><li><span>")+"</span></li></ul>")
+            $(".art-txt").html("<ul><li><span>"+$.map(d.textDiff, function(i){console.log(i); return decodeURI(i).replace(/\s*([:»;\?!%€])/g, '&nbsp;$1')}).join("</span></li><li><span>")+"</span></li></ul>")
 		}
 
 		$(".separator").append('<h4 class="law-title">'+data.law_title+'</h4>')
