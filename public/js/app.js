a99e3d9062740f6507fe5842e446857ac0f535ca
RouxRC <b.ooghe@gmail.com>
2014-05-11 20:50:34 +0200

    fix bug with mod2b display + split js load by module

--- public/js/app.js
+++ public/js/app.js
@@ -15,7 +15,7 @@ config(function ($routeProvider, $locationProvider) {
     .when('/mod0',{template: '<div mod0="mod0" class="padded mod0"></div>', controller: 'mainCtrl'})
     .when('/mod1',{template: '<div mod1="mod1" class="padded mod1"></div>', controller: 'mainCtrl'})
     .when('/mod2',{template: '<div mod2="mod2" class="padded mod2"></div>', controller: 'mainCtrl'})
-    .when('/mod2b',{template: '<div mod2b="mod2b" class="padded mod2b"></div>', controller: 'mainCtrl'})
+    .when('/mod2b',{template: '<div mod2b="mod2b" class="padded mod2"></div>', controller: 'mainCtrl'})
     .otherwise({redirectTo: '/mod0'});
     $locationProvider.html5Mode(true);
 });
