33ff41ad8b773e7473578ff1c4d2fb3b3d22eb5e
Tangui Morlier <tangui@tangui.eu.org>
2014-05-23 01:01:53 +0200

    Link to the bill explorer instead of the hoempage

--- public/js/app.js
+++ public/js/app.js
@@ -12,11 +12,10 @@ config(function ($routeProvider, $locationProvider) {
 
     .when('/',           {templateUrl: 'templates/home.html', controller: 'mainCtrl'})
     .when('/lois.html',       {template: '<div  mod0="mod0"  class="padded mod0"></div>', controller: 'mainCtrl'})
-    
-//    .when('/',              {template: '<div  mod0="mod0"  class="padded mod0"></div>', controller: 'mainCtrl'})
     .when('/loi.html',           {template: '<div  mod1="mod1"  class="padded mod1"></div>', controller: 'mainCtrl'})
     .when('/amendements.html',   {template: '<div  mod2="mod2"  class="padded mod2"></div>', controller: 'mainCtrl'})
     .when('/debats.html',        {template: '<div mod2b="mod2b" class="padded mod2"></div>', controller: 'mainCtrl'})
+    .when('/mod0',  {redirectTo: '/lois.html'})
     .when('/mod1',  {redirectTo: '/loi.html'})
     .when('/mod2',  {redirectTo: '/amendements.html'})
     .when('/mod2b', {redirectTo: '/debats.html'})
