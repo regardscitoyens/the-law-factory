1e2f34b7e335ce873c07644d00fc672ca55ae16a
RouxRC <b.ooghe@gmail.com>
2014-05-26 01:29:22 +0200

    tune title tooltip

--- public/js/directives.js
+++ public/js/directives.js
@@ -370,7 +370,7 @@ return {
                 var tit = upperFirst(data.long_title),
                     leg = "";
                 if (tit.length > 60) {
-                    leg = ' data-toggle="tooltip" data-placement="bottom" title="'+tit+'"';
+                    leg = ' data-toggle="tooltip" data-placement="right" title="'+tit+'"';
                     tit = scope.loi.substr(0,3).toUpperCase() + " " + upperFirst(data.short_title);
                 }
                 $(".title").html(
