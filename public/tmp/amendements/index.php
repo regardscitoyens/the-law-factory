fc1c5b09b1b3beaf7eb15120fc9155a18eaf9918
RouxRC <b.ooghe@gmail.com>
2015-02-03 02:01:04 +0100

    cleanup textes with no links

--- public/tmp/amendements/index.php
+++ public/tmp/amendements/index.php
@@ -16,7 +16,7 @@
       <h1>Proximité entre députés auteurs d'amendements identiques ou cosignés sur un texte donné</h1>
       <div class="buttons-container">Choisir un texte :
         <select name="loi" id="loi">
-<?php foreach (array("707","1005","1006","1108","1109","1473","1551","71","84","86","107","130","136","145","148","199","200","222","235","237","244","245","246","285","287","332","334","345","403","409","410","414","415","424","433","434","463","466","470","490","541","543","549","566","570","579","628","650","654","700","701","707","725","1329") as $loi) echo "<option>".$loi."</option>"; ?>
+<?php foreach (array("707","1005","1108","1109","1473","1551","71","84","86","107","145","148","199","200","222","235","244","245","246","287","332","345","403","409","410","414","415","424","433","434","463","466","490","541","543","549","566","570","579","628","650","654","700","701","707","725","1329") as $loi) echo "<option>".$loi."</option>"; ?>
         </select>
         <span id="loader"><img src="css/ajax-loader.gif" alt="loader" title="loader"/></span>
         <span id="menu">Basculer vers la vue <button class="hemicycle" id="layout">Cosignatures</button></span>
