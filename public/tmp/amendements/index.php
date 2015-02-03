f510111f0d66c402b45089a362b2f461263ac4d2
RouxRC <b.ooghe@gmail.com>
2015-02-03 01:33:42 +0100

    cleanup style

--- public/tmp/amendements/index.php
+++ public/tmp/amendements/index.php
@@ -19,7 +19,7 @@
 <?php foreach (array("707","1005","1006","1108","1109","1473","1551","71","84","86","107","130","136","145","148","199","200","222","235","237","244","245","246","285","287","332","334","345","403","409","410","414","415","424","433","434","463","466","470","490","541","543","549","566","570","579","628","650","654","700","701","707","725","1329") as $loi) echo "<option>".$loi."</option>"; ?>
         </select>
         <span id="loader"><img src="css/ajax-loader.gif" alt="loader" title="loader"/></span>
-        <span id="menu">Basculer vers la vue <button class="btn hemicycle" id="layout">Cosignatures</button></span>
+        <span id="menu">Basculer vers la vue <button class="hemicycle" id="layout">Cosignatures</button></span>
       </div>
       <div id="sigma-parent">
         <div id="sigma"></div>
