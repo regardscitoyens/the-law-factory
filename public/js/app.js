fefc7fffb7028f5451848ffcc3c65cf794215cbb
François Massot <fmassot@yanport.com>
2016-01-01 20:37:44 +0100

    Use string_to_slug as a filter

--- public/js/app.js
+++ public/js/app.js
@@ -69,4 +69,22 @@ angular.module('theLawFactory', [
         var d2 = d.split('-');
         return d2[2] + "/" + d2[1] + "/" + d2[0];
     }
+}).filter('slug', function() {
+    return function (str) {
+        str = str.replace(/^\s+|\s+$/g, ''); // trim
+        str = str.toLowerCase();
+
+        // remove accents, swap ñ for n, etc
+        var from = "àáäâèéëêìíïîòóöôùúüûñç·/_,:;";
+        var to =   "aaaaeeeeiiiioooouuuunc------";
+        for (var i = 0, l = from.length; i < l; i++) {
+            str = str.replace(new RegExp(from.charAt(i), 'g'), to.charAt(i));
+        }
+
+        str = str.replace(/[^a-z0-9 -]/g, '') // remove invalid chars
+            .replace(/\s+/g, '-') // collapse whitespace and replace by -
+            .replace(/-+/g, '-'); // collapse dashes
+
+        return str;
+    };
 });
--- public/js/controllers.js
+++ public/js/controllers.js
@@ -111,24 +111,6 @@ angular.module('theLawFactory.controllers', ['theLawFactory.config']).
                     .replace(/\W/g, '-')
         };
 
-        $scope.string_to_slug = function (str) {
-            str = str.replace(/^\s+|\s+$/g, ''); // trim
-            str = str.toLowerCase();
-
-            // remove accents, swap ñ for n, etc
-            var from = "àáäâèéëêìíïîòóöôùúüûñç·/_,:;";
-            var to = "aaaaeeeeiiiioooouuuunc------";
-            for (var i = 0, l = from.length; i < l; i++) {
-                str = str.replace(new RegExp(from.charAt(i), 'g'), to.charAt(i));
-            }
-
-            str = str.replace(/[^a-z0-9 -]/g, '') // remove invalid chars
-                .replace(/\s+/g, '-') // collapse whitespace and replace by -
-                .replace(/-+/g, '-'); // collapse dashes
-
-            return str;
-        };
-
         $scope.shortenString = function (s, n) {
             if (s.length > n) {
                 s = s.substr(0, s.indexOf(' ', n - 20)) + "…";
