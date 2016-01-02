2eb4a6d74de934d8056281c413484e28809afd4e
François Massot <fmassot@yanport.com>
2016-01-02 01:31:00 +0100

    Log when an api service is used

--- public/js/services.js
+++ public/js/services.js
@@ -21,29 +21,57 @@ angular.module('theLawFactory.services', ['theLawFactory.config'])
             }
         };
     })
-    .factory('api', function ($http, $q, apiService, API_ROOT_URL) {
+    .factory('api', function ($http, $q, $log, apiService, API_ROOT_URL) {
         if (API_ROOT_URL.substr(-1) != "/") API_ROOT_URL += "/";
         var api = {
             getLawlist: function () {
-                return apiService.getDataSample(API_ROOT_URL + 'dossiers_promulgues.csv');
+                return apiService.getDataSample(API_ROOT_URL + 'dossiers_promulgues.csv')
+                    .then(function(data) {
+                        $log.debug('law list loaded');
+                        return data;
+                    });
             },
             getProcedure: function (id) {
-                return apiService.getDataSample(API_ROOT_URL + id + '/viz/procedure.json');
+                return apiService.getDataSample(API_ROOT_URL + id + '/viz/procedure.json')
+                    .then(function(data) {
+                        $log.debug('procedure loaded', data);
+                        return data;
+                    });
             },
             getArticle: function (id) {
-                return apiService.getDataSample(API_ROOT_URL + id + '/viz/articles_etapes.json');
+                return apiService.getDataSample(API_ROOT_URL + id + '/viz/articles_etapes.json')
+                    .then(function(data) {
+                        $log.debug('articles loaded', data);
+                        return data;
+                    });
             },
             getAmendement: function (id, step) {
-                return apiService.getDataSample(API_ROOT_URL + id + '/viz/amendements_' + step + '.json');
+                return apiService.getDataSample(API_ROOT_URL + id + '/viz/amendements_' + step + '.json')
+                    .then(function(data) {
+                        $log.debug('amendements loaded', data);
+                        return data;
+                    });
             },
             getIntervention: function (id) {
-                return apiService.getDataSample(API_ROOT_URL + id + '/viz/interventions.json');
+                return apiService.getDataSample(API_ROOT_URL + id + '/viz/interventions.json')
+                    .then(function(data) {
+                        $log.debug('interventions loaded', data);
+                        return data;
+                    });
             },
             getDossiers: function () {
-                return apiService.getDataSample(API_ROOT_URL + 'dossiers_0_49.json');
+                return apiService.getDataSample(API_ROOT_URL + 'dossiers_0_49.json')
+                    .then(function(data) {
+                        $log.debug('dossiers loaded', data);
+                        return data;
+                    });
             },
             getTutorials: function () {
-                return apiService.getDataSample('tutorial.json');
+                return apiService.getDataSample('tutorial.json')
+                    .then(function(data) {
+                        $log.debug('tutorials loaded', data);
+                        return data;
+                    });
             }
         };
         return api;
