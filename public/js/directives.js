467a2a1ff65c4421611dfdb7f526b6f9bd43e81c
Nicolas Joyard <joyard.nicolas@gmail.com>
2016-01-21 22:15:41 +0100

    Move mod2b to debats module

--- public/js/directives.js
+++ public/js/directives.js
@@ -36,35 +36,6 @@ var accentMap = {
 /* Directives */
 
 angular.module('theLawFactory.directives', [])
-    .directive('mod2b', ['api', '$rootScope', '$location', '$compile',
-        function (api, $rootScope) {
-            return {
-                restrict: 'A',
-                replace: false,
-                templateUrl: 'templates/mod2b.html',
-                controller: function ($scope) {
-                    $scope.step = 0;
-                    $scope.mod = "mod2b";
-                    $scope.setHelpText("Chaque boîte représente un groupe d'orateurs intervenus dans les débats sur un sujet. La longueur indique le nombre de mots prononcés et la couleur le groupe politique. Cliquez sur une boîte pour voir la liste des orateurs et consulter le texte des débats.");
-                    $scope.vizTitle = "DÉBATS";
-
-                    update();
-
-                    function update() {
-                        thelawfactory.utils.spinner.start();
-                        if ($scope.etape != null) {
-                            api.getIntervention($scope.loi).then(function (data) {
-                                $scope.data = data;
-                                $rootScope.pageTitle = $rootScope.lawTitle + " - Débats | ";
-                                init(data, $scope.etape, $scope.vizTitle, $scope.helpText);
-                            }, function () {
-                                $scope.display_error("impossible de trouver les interventions pour ce texte à cette étape");
-                            })
-                        }
-                    }
-                }
-            };
-        }])
     .directive('lawlist', ['$location', '$rootScope', 'api', function ($location, $rootScope, api) {
             return {
                 restrict: 'A',
