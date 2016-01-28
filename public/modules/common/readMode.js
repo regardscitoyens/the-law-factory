63d33fb1416a8c5f58edbb1aa66d715fbad1398b
Nicolas Joyard <joyard.nicolas@gmail.com>
2016-01-28 18:36:34 +0100

    Save read mode in URL

--- public/modules/common/readMode.js
+++ public/modules/common/readMode.js
@@ -1,21 +1,29 @@
 'use strict';
 
 angular.module('theLawFactory')
-.directive('readMode', function() {
+.directive('readMode', ['$location', function($location) {
     return {
         restrict: 'A',
         controller: function($scope) {
-            $scope.read = false;
+            $scope.read = $location.search()['read'] === '1';
+
             $scope.readmode = function () {
                 $(".text").css({"width": "93.43%", "left": "3.3%"});
                 $(".gotomod").addClass('readmode');
+                $location.search('read', '1');
                 $scope.read = true;
             };
+
             $scope.viewmode = function () {
                 $(".text").css({"width": "23.40%", "left": "73.3%"});
                 $(".gotomod").removeClass('readmode');
+                $location.search('read', null);
                 $scope.read = false;
             };
+
+            if ($scope.read) {
+                $scope.readmode();
+            }
         }
     }
-});
+}]);
